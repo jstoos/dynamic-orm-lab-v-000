6c15c203185c43da8237ddb3411050d49e9b6896
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-18 00:52:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -72,8 +72,8 @@ class InteractiveRecord
       sql = <<-SQL
         SELECT * FROM #{self.table_name} WHERE "#{key.to_s}" = "#{value.to_s}"
       SQL
-      row = DB[:conn].execute(sql)
-      row
+      DB[:conn].execute(sql)
+      
     end
   end
 
