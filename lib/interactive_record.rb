5c97b37b4c4c4225e7f6f24c7d87d928419991e6
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-18 00:51:35 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -68,7 +68,7 @@ class InteractiveRecord
     #   SELECT * FROM #{self.table_name} WHERE '#{key}.to_s' = "'#{value}.to_s'"
 
     random_attribute_hash.each do |key, value|
-      binding.pry
+      #binding.pry
       sql = <<-SQL
         SELECT * FROM #{self.table_name} WHERE "#{key.to_s}" = "#{value.to_s}"
       SQL
